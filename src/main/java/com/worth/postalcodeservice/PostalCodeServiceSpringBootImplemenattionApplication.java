22cb4b12e356c4aa05d6d9c4e9e5c6eb5da850b1
jalsordo <jalsordo@TTU-6SLNMH2.usersad.everis.int>
2019-05-18 17:31:04 +0000

    initial commit.

---
+++ src/main/java/com/worth/postalcodeservice/PostalCodeServiceSpringBootImplemenattionApplication.java
@@ -0,0 +1,13 @@
+package com.worth.postalcodeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostalCodeServiceSpringBootImplemenattionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostalCodeServiceSpringBootImplemenattionApplication.class, args);
+	}
+
+}
